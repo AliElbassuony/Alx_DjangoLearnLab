e245dde809b9d16025894ec4ff8c6dc3e0b460d9
Ali <alimohamed19499@gmail.com>
2025-07-21 00:18:41 +0300

    update views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book, Library
+from .models import Library, Book
 from django.views.generic.detail import DetailView
 
 def home(request):
