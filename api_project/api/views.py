171ddba9289a89666459341bf6c449f24f3966b7
Ali <alimohamed19499@gmail.com>
2025-08-09 22:54:25 +0300

    update

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 # Create your views here.
 
+# This ViewSet requires users to be authenticated via TokenAuthentication.
+# Only authenticated users can access endpoints.
+# Token is obtained by POSTing username and password to /api-token-auth/
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
