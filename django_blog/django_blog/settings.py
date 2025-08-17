45530b7f761a7cf280fb59584a5a02e41d82bf88
Ali <alimohamed19499@gmail.com>
2025-08-17 23:58:15 +0300

    update

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "blog/static")]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':  [os.path.join(BASE_DIR, "blog/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
