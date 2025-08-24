2c57e9c6c868e7ba71c2fe36dc149ea636fa5270
Ali <alimohamed19499@gmail.com>
2025-08-25 00:59:41 +0300

    update tag

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -30,5 +30,6 @@ urlpatterns = [
 
     path("search/", search_posts, name="search_posts"),
     path("tags/<str:tag_name>/", posts_by_tag, name="posts_by_tag"),
+    path("tags/<slug:tag_slug>/", views.PostByTagListView.as_view(), name="posts_by_tag"),
 ]
 
