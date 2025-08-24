77a354edf79f66a51185f935471766fba4fc0b18
Ali <alimohamed19499@gmail.com>
2025-08-25 00:17:10 +0300

    update blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegisterForm
+
 
 # Create your views here.
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = RegisterForm()
+    return render(request, "blog/register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = AuthenticationForm()
+    return render(request, "blog/login.html", {"form": form})
+
+@login_required
+def profile_view(request):
+    return render(request, "blog/profile.html", {"user": request.user})
