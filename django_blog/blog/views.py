51e2de4fedd0bf1eceb9b0d3c1da2884af1d1c5a
Ali <alimohamed19499@gmail.com>
2025-08-25 01:00:11 +0300

    update tag

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -12,6 +12,22 @@ from .forms import PostForm
 from django.db.models import Q
 
 # Create your views here.
+
+class PostByTagListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        tag_slug = self.kwargs.get("tag_slug")
+        return Post.objects.filter(tags__slug=tag_slug).distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tag_slug"] = self.kwargs.get("tag_slug")
+        return context
+    
+    
 class CommentCreateView(LoginRequiredMixin, CreateView):
     model = Comment
     form_class = CommentForm
